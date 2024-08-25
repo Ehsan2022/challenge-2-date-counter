8a11f1e70ac7af1f67be84c47f942907975add26
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2024-08-25 23:55:17 +0430

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Date Counter</h1>
+      <p>Update count to find date</p>
+      <hr/>
+      <Counter></Counter>
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [steps, setSteps] = useState(1);
+  const [counts, setCounts] = useState(0);
+
+  function incStep() {
+    setSteps((s) => s + 1);
+  }
+  function dicStep() {
+    setSteps((s) => s - 1);
+  }
+  function incCount() {
+    setCounts((c) => c + steps);
+  }
+  function dicCount() {
+    setCounts((c) => c - steps);
+  }
+
+  const date = new Date();
+  date.setDate(date.getDate() + counts);
+
+  return (
+    <>
+      <div>
+        <button onClick={dicStep}>-</button>
+        <span>Step : {steps}</span>
+        <button onClick={incStep}>+</button>
+      </div>
+      <div>
+        <button onClick={dicCount}>-</button>
+        <span>Count : {counts}</span>
+        <button onClick={incCount}>+</button>
+      </div>
+      <div className="text">
+        <span>
+          {counts === 0
+            ? "Today is "
+            : counts > 0
+            ? `${counts} days from today is `
+            : `${Math.abs(counts)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </div>
+    </>
+  );
+}
